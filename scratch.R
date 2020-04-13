eb318df306c56d5d35ec3abd3fa6184fd62c0b1a
lobes <samuel.t.giles@gmail.com>
2020-04-13 16:37:41 +0800

    update scratch with freq solution

--- scratch.R
+++ scratch.R
@@ -1,6 +1,53 @@
 library(quanteda)
 library(data.table)
 library(readtext)
+library(dplyr)
+library(tokenizers)
+library(stringi)
 
-test_corpus <- corpus(readtext("./data/sample/twitter_sample.txt"))
-test_dfm <- dfm(test_corpus, ngrams = 2, remove_punct = TRUE)
+sample <- "./data/sample/twitter_sample.txt"
+
+ngram <- sample %>%
+    readLines %>%
+    tokenize_ngrams(lowercase = TRUE,
+                    n = 5,
+                    n_min = 2) %>%
+    unlist(use.names = FALSE) %>%
+    data.table
+
+names(ngram) <- c("ngram")
+
+freq <- ngram[, .N, by = "ngram"]
+
+# test_dt <- sample %>%
+#     readLines %>%
+#     data.table
+# 
+# text <- sample %>%
+#     readLines
+# 
+# 
+# ngrams <- test_dt %>%
+#     tokenize_ngrams(lowercase = TRUE,
+#                     n = 5,
+#                     n_min = 2)
+#     
+# 
+# test_text_obj <- sample %>%
+#     readLines() %>%
+#     paste0() %>%
+#     tolower()
+# 
+# 
+
+# test_corpus <- corpus(test_text_obj)
+# 
+# test_tokens <- test_text_obj %>%
+#     tokens(what = "word",
+#            remove_punct = TRUE,
+#            remove_symbols = TRUE,
+#            remove_numbers = FALSE,
+#            remove_url = FALSE,
+#            remove_separators = TRUE)
+# 
+# test_dfm <- dfm(test_corpus)
